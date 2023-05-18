ec61a75b63a79ea63ba0731042d1918c944e84e3
vamsisiddireddy <104156804+vamsisiddireddy@users.noreply.github.com>
2023-05-18 12:29:19 +0530

    Update Api.js

--- Motivityhospital/ml-hospital-react/src/Utils/Api/Api.js
+++ Motivityhospital/ml-hospital-react/src/Utils/Api/Api.js
@@ -1,9 +1,4 @@
 
 
-//local api
+//backend url
  export const baseURL ='http://localhost:8080/'
-// Server Api
-// export const baseURL ='https://ml-hospital-mngmt.azurewebsites.net/'
-//vm Api
-//export const baseURL ='http://172.174.209.218:8080/'
-//export const baseURL ='https://f577-182-76-136-42.in.ngrok.io/'
