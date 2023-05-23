0ef5b2e5dc08cda01705fceb89885bc8027f0780
vamsisiddireddy <104156804+vamsisiddireddy@users.noreply.github.com>
2023-05-23 11:40:37 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Check if git command is available
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed."
-    echo "git is installing."
-    yum install -y git
-else
-    echo "Git is installed."
-fi
-#!/bin/bash
+#if ! command -v git &> /dev/null; then
+#    echo "Git is not installed."
+#    echo "git is installing."
+#   yum install -y git
+#else
+#    echo "Git is installed."
+#fi
 
 # Check if Terraform command is available
 if ! command -v terraform &> /dev/null; then
